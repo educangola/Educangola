0c9721dfd54e433e466b6d6b3c47483d92bd51c8
Carlos muquissi zangui <161896848+004charles@users.noreply.github.com>
2024-12-23 15:19:09 +0100

    Update settings.py

--- ANGOLA_EDUCA/settings.py
+++ ANGOLA_EDUCA/settings.py
@@ -114,7 +114,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Diretório onde você tem arquivos estáticos
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
