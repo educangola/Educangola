2ef571b2c07e0e5c8e8d7d84912dbb80ae3e334d
muquissi <muquissicarlos@gmail.com>
2025-01-25 21:53:41 +0100

    whitenoise

--- ANGOLA_EDUCA/settings.py
+++ ANGOLA_EDUCA/settings.py
@@ -75,6 +75,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ANGOLA_EDUCA.wsgi.application'
 ASGI_APPLICATION = 'ANGOLA_EDUCA.asgi.application'
 
+#============================AWS=============================
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
